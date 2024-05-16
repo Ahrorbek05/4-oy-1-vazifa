cbf8da5e7e93b2746f34a4028e7213a7e8a37848
Ahrorbek05 <akramjonovahrorbek@gmail.com>
2024-05-16 20:56:26 +0500

    js first work write

---
+++ js/index.js
@@ -0,0 +1,86 @@
+1-masala
+let = L +prompt("Uzunlik sm da kiriting");
+let metr  = Math.floor(L / 100);
+console.log(metr);
+
+
+2-masala
+let = m + prompt("Og'irlikni kgda kiriting");
+let tonna = Math.floor (m / 1000);
+console.log(tonna);
+
+3-masala
+let = f + prompt("baytlarda kiriting");
+let kbayt = Math.floor(f / 1024);
+console.log(kbayt);
+
+4-masala
+alert("a>b");
+let a = +prompt("a kesma");
+let b = +prompt("b kesma");
+let c = Math.floor(a / b);
+console.log(c);
+
+5-masala
+alert("a>b")
+let A = +prompt('a kesma')
+let B = +prompt('b kesma')
+let C = Math.floor(A%B);
+console.log(C);
+
+6-masala
+alert("ab");
+let ab = +prompt("ikki xonali son"); 
+let w = Math.floor(ab/10);
+let clutch = Math.floor(ab%10);
+console.log(w);
+console.log(clutch);
+
+7-masala
+alert('n ikki xonali son')
+let n = +prompt('n ikki xonali son');
+let s = Math.floor(n/10);
+let f = Math.floor(n%10);
+let won = Math.floor(s+f);
+console.log(won);
+
+8-masala
+let p = +prompt('p ikki xonali son kiriting');
+let o = Math.floor(p/10);
+let bt = Math.floor(p%10);
+let P = Math.floor((bt*10)+o);
+console.log(P);
+
+9-masala
+let ac = +prompt('ac uch xonali son');
+let ch = Math.floor(ac/100);
+console.log(ch);
+
+10-masala
+let ad = +prompt("a")
+let bc = Math.floor(ad%10)
+let g = Math.floor(ad/10%10);
+console.log(bc);
+console.log(g);
+
+11-masala
+let wp = +prompt('uch xonali son kiriting');
+let ap = Math.floor(wp%10);
+let k = Math.floor(wp/10%10);
+let m = Math.floor(wp/100);
+console.log(ap+k+m);
+
+12-masala
+let st = prompt("uch xonali son");
+let r = Math.floor(st%10);
+let i = Math.floor(st/10%10);
+let cv = Math.floor(st/100);
+console.log(r,i,cv);
+
+13-masala
+alert('uch xonali son');
+let t = +prompt('t');
+let cp = Math.floor(t/100);
+let pc = Math.floor(t/10%10);
+let O = Math.floor(t%10);
+console.log((pc*100)+(O*10)+cp);
